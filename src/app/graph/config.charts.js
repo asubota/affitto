1fcc07dcad4734e5d25d4a4270df6dd5a122ebe6
Aleksandr <subotaav@gmail.com>
2015-06-13 18:55:45 +0300

    CSS transitions

--- src/app/graph/config.charts.js
+++ src/app/graph/config.charts.js
@@ -12,10 +12,6 @@
     ChartJsProvider.setOptions({
       colours: ['#F7464A', '#97BBCD', '#FDB45C', '#46BFBD', '#DCDCDC', '#949FB1', '#4D5360']
     });
-
-    // ChartJsProvider.setOptions('Pie', {
-    //   colours: ['#F7464A','#97BBCD', '#FDB45C', '#46BFBD', '#DCDCDC', '#949FB1', '#4D5360']
-    // });
   }
 
 })();
